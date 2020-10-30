0623c69f557558068e49727db89187fde85fe741
Bill Jensen <jensendesign@users.noreply.github.com>
2020-10-30 18:17:04 -0400

    exercise 1

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let js = "amazing";
+// let js = "amazing";
+// console.log(49 + 4 + 112);
 
-if (js === "amazing") alert("Javascript is Fun!");
+// console.log('Jonas');
+// console.log(23);
 
-var answer = 49 + 4 + 112;
-console.log(answer);
+let country = 'USA';
+let continent = 'North America';
+let population = '300';
+
+console.log(country);
+console.log(continent);
+console.log(population + 'M');
