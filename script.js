214bfc7227d1a91634ce2fbe7d7320bb2f56dad4
Bill Jensen <jensendesign@users.noreply.github.com>
2020-10-30 18:38:39 -0400

    datatype

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// let js = "amazing";
-// console.log(49 + 4 + 112);
+/* 
+let js = "amazing";
+console.log(49 + 4 + 112);
 
-// console.log('Jonas');
-// console.log(23);
+console.log('Jonas');
+console.log(23);
 
 let country = 'USA';
 let continent = 'North America';
@@ -10,4 +11,16 @@ let population = '300';
 
 console.log(country);
 console.log(continent);
-console.log(population + 'M');
+console.log(population + 'M'); 
+
+var myJava = 2;
+console.log(myJava);
+console.log(typeof myJava);
+
+var myUndefined;
+console.log(myUndefined);
+console.log(typeof myUndefined);
+
+console.log(typeof null);
+*/
+
