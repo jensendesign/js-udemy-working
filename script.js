daf2fffd317d430f720e5f081f5748fbe6c5484b
Bill Jensen <jensendesign@users.noreply.github.com>
2020-10-30 20:48:09 -0400

    operators

--- script.js
+++ script.js
@@ -22,5 +22,57 @@ console.log(myUndefined);
 console.log(typeof myUndefined);
 
 console.log(typeof null);
+
+
+let age = 30;
+// mutate variables 
+age = 31;
+
+const birthYear = 1967;
+
+
+// math operators
+const now = 2037;
+const ageJohn = now - 1991;
+const ageSara = now - 1943;
+console.log(ageJohn, ageSara);
+console.log(ageJohn * 2, ageJohn / 10, 2 ** 3);
+
+const person1 = 'Bill';
+const person2 = 'Steve';
+console.log(person1 + " " + person2);
+
+// assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; //adding 1 101
+x--; //minus 1 100
+console.log(x);
+
+// comparison operators
+console.log(ageJohn < ageSara); // >, <, >=, <=
+console.log(ageSara >= 95);
+
+// exercise
+const population = 300000000;
+console.log(population / 2);
+console.log(population + 1);
+console.log(population > 6000000);
+console.log(3300000 > population);
+const description = 'Portugal is in Europe, and its 11 million people speak portuguese';
 */
 
+const now = 2037;
+const ageJohn = now - 1991;
+const ageSara = now - 1943;
+
+console.log(now - 1991 > now - 1943);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const averageAge = (ageJohn + ageSara) / 2;
+console.log(ageJohn, ageSara);
+console.log(averageAge);
