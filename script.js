c3eaba96e643abe43a575512b65df75e45e4a393
Bill Jensen <jensendesign@users.noreply.github.com>
2020-11-02 23:01:57 -0500

    arrays

--- script.js
+++ script.js
@@ -134,3 +134,90 @@ HINT: To check if number A is at least double number B, check for A >= 2 * B. Ap
 
 GOOD LUCK 😀
 */
+
+/*
+const friend1 = 'Mike';
+const friend2 = 'Steve';
+const friend3 = 'Peter';
+
+const friends = ['Mike', 'Steve', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 2102, 1833, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length -1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Bill'
+const bill = [firstName, 'Jensen', 2020 - 1965, 'designer', friends];
+console.log(bill);
+
+// exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+  console.log(ages);
+  */
+
+  /*
+  // add elements
+const friends = ['Mike', 'Steve', 'Peter'];
+const newLength = friends.push('Bob'); // also returns length
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('Tom');
+console.log(friends);
+
+// remove elements
+friends.pop(); // last
+const popped = friends.pop(); // returns removed element
+console.log(friends);
+console.log(popped);
+
+const shifted = friends.shift();
+console.log(friends);
+console.log(shifted);
+
+console.log(friends.indexOf('Steve'));
+
+console.log(friends.includes('Steve'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('steve')); // strict equality
+
+if (friends.includes('Mike')) {
+  console.log('You have a friend called Mike');
+}
+*/
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
