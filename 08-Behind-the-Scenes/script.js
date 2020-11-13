1ae7d1f0399ccd174dcace124c18c7a61136df9e
Bill Jensen <jensendesign@users.noreply.github.com>
2020-11-12 21:14:30 -0500

    Update script.js

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let outPut = `${firstName} is ${age} born in ${birthYear}`;
+    console.log(outPut);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Create NEW vars
+      const firstName = 'Mark'; // different var with same name - diff scope **
+      const str = `Oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+
+      //   function add(a, b) {
+      //     return a + b;
+      //   }
+      // Redefine var
+      outPut = 'NEW OUTPUT';
+    }
+    console.log(millenial);
+    console.log(outPut);
+    // console.log(add(1, 2));
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Bill';
+
+calcAge(1991);
